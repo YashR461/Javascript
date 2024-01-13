d465c7998287beb66d54f16fd202ac8d34756d95
YashR461 <rastogiyash461@gmail.com>
2024-01-13 19:53:42 +0530

    datatyopes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all JS code as newer version
+
+
+// number => represents an integer value
+// string => ""
+// boolean => represents a bool value
+// null => standalone value
+// symbol => unique
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
