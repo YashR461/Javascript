86c4d2cccb4172d62bff3efd8cf3d951453ce001
YashR461 <rastogiyash461@gmail.com>
2024-01-13 21:04:01 +0530

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,14 @@
+// on numbers
+let num1 = 3
+let num2 = -(num1)
+console.log(num2)
+
+// concatenation on strings
+let str1 = "hello"
+let str2 = " yash"
+console.log(str1 + str2)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log(1 + 2 + "2")
+console.log("1" + 2 + 2)
