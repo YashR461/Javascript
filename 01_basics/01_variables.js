ec1562e4c9c3200ba4ee53d7225b69bc3def5ecc
YashR461 <rastogiyash461@gmail.com>
2024-01-13 19:35:16 +0530

    Lecture on let const and var.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 14455
+let accountEmail = "yash@gmail.com"
+var accountPassword = "Jaipur"
+let accountState
+
+console.log(accountEmail)
+console.table([accountEmail, accountId, accountPassword, accountState])
