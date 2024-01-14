e9fc4c7476e27528fe9e207636c137627365b18f
YashR461 <rastogiyash461@gmail.com>
2024-01-14 20:22:52 +0530

    Datatypes Revisited

---
+++ 01_basics/06_datatypesRevisited.js
@@ -0,0 +1,32 @@
+// Primitive (call by value)
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+//Non-Primitive(call by reference)
+// Array
+// Object
+// Function
+
+// array
+let superHeroes = ["Yash" , "Javascript"]
+
+//object
+let myObj = {
+    name : "Yash",
+    age : 21
+}
+
+// function declaration
+const myFunction = function(){
+    console.log("Hello Yash here")
+}
+
+// function call
+myFunction()
+
+console.log(typeof myFunction)
