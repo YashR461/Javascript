7549ce0620ab3fb8eb84eddcddf81418cc62a5de
YashR461 <rastogiyash461@gmail.com>
2024-02-18 19:45:06 +0530

    while and do while loops in javascript

---
+++ 05_iterations/02_whileAndDoWhile.js
@@ -0,0 +1,13 @@
+//while loops
+let myArray = ["Batman", "Spiderman", "Superman"];
+let i = 0;
+while(i < myArray.length) {
+    console.log(`At index ${i} the superHero is ${myArray[i]}`)
+    i++
+}
+
+//do while loops
+let score = 11;
+do {
+    console.log(`Score is ${score}`)
+} while (score <= 10);
