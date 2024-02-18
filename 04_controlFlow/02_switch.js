7c80941f8e36425165be68ecb410120af9bac62e
YashR461 <rastogiyash461@gmail.com>
2024-02-18 18:52:33 +0530

    switch case in javascript

---
+++ 04_controlFlow/02_switch.js
@@ -0,0 +1,17 @@
+let month = 2
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("February")
+        break;
+    case 3:
+        console.log("March")
+        break;
+
+    default:
+        console.log("Default case")
+        break;
+}
