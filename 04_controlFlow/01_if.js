c4cff43e72a8246de4400d5312549b872e424efb
YashR461 <rastogiyash461@gmail.com>
2024-02-18 18:52:18 +0530

    if statements

---
+++ 04_controlFlow/01_if.js
@@ -0,0 +1,24 @@
+if (true) {
+    console.log("Yash Rastogi")
+}
+if(false) {
+    console.log("Will not run")
+}
+
+// if else
+let check = 1
+if(check === 1) {
+    console.log("Check is 1")
+}else{
+    console.log("Check is not 1")
+}
+
+//if elseif else
+let max = 10
+if (max === 100) {
+    console.log("Max is 100")
+}else if(max === 150) {
+    console.log("Max is 150")
+}else{
+    console.log("Some value")
+}
