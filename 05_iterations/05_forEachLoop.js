b9052cdae31200a9acc470b8e03a7e97d71d36cb
YashR461 <rastogiyash461@gmail.com>
2024-02-19 10:41:04 +0530

    for each loop updated

--- 05_iterations/05_forEachLoop.js
+++ 05_iterations/05_forEachLoop.js
@@ -1,5 +1,4 @@
 // for each loop
-
 const coding = ["js", "golang", "python", "cpp"]
 
 //regular function
@@ -12,7 +11,28 @@ coding.forEach((value)=>{
     console.log(value)
 ;})
 
-//for each doesn't only bring item or val in parameter but it brings
+//for each doesn't only bring item or val in parameter but it also brings index and entire array
 coding.forEach( (item, index, arr) => {
     console.log(item, index, arr);
+});
+
+//Accessing objects in array using foreach loop
+const myCoding = [
+    {
+        languageName: "Javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "GoLang",
+        languageFileName: "go"
+    }
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageFileName);
+    console.log(item.languageName);
 });
