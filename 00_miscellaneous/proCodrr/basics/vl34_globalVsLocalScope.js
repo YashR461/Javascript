1cc71aab0175203dead09412583841e16019c0a7
YashR461 <rastogiyash461@gmail.com>
2024-03-28 23:36:53 +0530

    global and local variables

---
+++ 00_miscellaneous/proCodrr/basics/vl34_globalVsLocalScope.js
@@ -0,0 +1,21 @@
+const username = "Yash"
+const age = 21
+var a = 50 
+
+function add() {
+    const username = "Yash Rastogi"
+    const x = 5;
+    const y = 8;
+    console.log(x+y)
+    console.log(username)
+}
+
+function subtract() {
+    const x = 15;
+    const y = 18;
+    console.log(x-y)
+    console.log(username)
+}
+
+add()
+subtract()
