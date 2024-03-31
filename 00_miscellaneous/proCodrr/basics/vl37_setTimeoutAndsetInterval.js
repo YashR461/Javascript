e3f0386e4f5b770af5a010d2e9fb4924ff58e610
YashR461 <rastogiyash461@gmail.com>
2024-04-01 00:02:22 +0530

    set timeout

---
+++ 00_miscellaneous/proCodrr/basics/vl37_setTimeoutAndsetInterval.js
@@ -0,0 +1,9 @@
+// setTimeout( ()=> {
+//     console.log("Yash Rastogi")
+// }, 2000);
+
+const printMyName = setTimeout ( () => {
+    console.log("My name is Yash Rastogi")
+}, 4000)
+
+clearTimeout(printMyName);
