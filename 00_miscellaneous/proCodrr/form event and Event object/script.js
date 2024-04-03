7683118823079a2481ebcdc4bc2c47f9a6ccbf3f
YashR461 <rastogiyash461@gmail.com>
2024-04-03 22:36:16 +0530

    form event basics

---
+++ 00_miscellaneous/proCodrr/form event and Event object/script.js
@@ -0,0 +1,31 @@
+const usernameInput = document.getElementById('username');
+
+//click event
+// usernameInput.addEventListener('click', function() {
+//     console.log(`Input is clicked`);
+// })
+
+//Input Event
+// usernameInput.addEventListener('input', function(event) {
+//     // console.log(event.target.value);
+//     document.querySelector('p').innerText = event.target.value
+//     // console.log(event);
+// })
+
+//Change Event
+// usernameInput.addEventListener('change', (event) => {
+//     console.log(`Event Triggered`)
+//     document.querySelector('p').innerText = event.target.value
+// })
+
+//Focus Event
+// usernameInput.addEventListener('focus', (event) => {
+//     console.log(`Focus event triggered`)
+// })
+
+// //Submit Event
+// const btn = document.getElementById('button');
+// console.log(btn)
+// btn.addEventListener('submit', () => {
+//     console.log(`Submit event is triggered`)
+// })
